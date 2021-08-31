91ec800259a4e9e155f8b3d2db6a404fe88ac143
ilkka <ilkka.o.pekkala@gmail.com>
2021-08-31 15:23:04 +0300

    Function customeradd almost ready

---
+++ functions/customeradd/main.py
@@ -0,0 +1,34 @@
+def getsecret(secretname, version):
+    import google.cloud.secretmanager as secretmanager
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/week10-1-324606/secrets/{secretname}/versions/{version}"
+    response = client.access_secret_version(request={"name": name})
+    return response.payload.data.decode("UTF-8")
+
+def customeradd(request):
+    import psycopg2
+    dbname = getsecret("dbname", 1)
+    user = "postgres"
+    password = getsecret("dbpassword", 1)
+    host = '/cloudsql/week10-1-324606:us-central1:petshop'
+    conn = None
+    request_json = request.get_json(silent=True)
+    #id = request_json.get("id")
+    name = request_json.get("name")
+    address = request_json.get("address")
+    email = request_json.get("email")
+    SQL = "INSERT INTO customer(name, address, email) VALUES (%s,%s,%s);"
+    result = "Insert failed"
+    try:
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user,  password=password)
+        cursor = conn.cursor()
+        cursor.execute(SQL, (name, address, email))
+        conn.commit()
+        cursor.close()
+        result = "Insert success"
+    except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+    finally:
+        if conn is not None:
+            conn.close()      
+    return result
