b757811a2113f38d153af98f4de45db47691400e
ilkka <ilkka.o.pekkala@gmail.com>
2021-08-31 13:57:46 +0300

    Finished with itemgetwarehouse

---
+++ functions/itemgetwarehouse/main.py
@@ -0,0 +1,33 @@
+def getsecret(secretname, version):
+    import google.cloud.secretmanager as secretmanager
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/week10-1-324606/secrets/{secretname}/versions/{version}"
+    response = client.access_secret_version(request={"name": name})
+    return response.payload.data.decode("UTF-8")
+
+def itemgetwarehouse(request):
+    import psycopg2
+    dbname = getsecret("dbname", 1)
+    user = "postgres"
+    password = getsecret("dbpassword", 1)
+    host = '/cloudsql/week10-1-324606:us-central1:petshop'
+    conn = None
+    id = request.args.get('id')
+    SQL = f"SELECT name, amount FROM warehouse WHERE id={id};"
+    results = []
+    try:
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user,  password=password)
+        cursor = conn.cursor()
+        cursor.execute(SQL)
+        #conn.commit()
+        row = cursor.fetchone()
+        while row is not None:
+            results.append(str(row))
+            row = cursor.fetchone()
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+    finally:
+        if conn is not None:
+            conn.close()      
+    return "\n".join(results)
