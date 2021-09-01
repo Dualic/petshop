ba4e58a81388593fce96d8025d2d554acd3dc9b8
ilkka <ilkka.o.pekkala@gmail.com>
2021-09-01 11:24:21 +0300

    getall functions needed to initialize a nested dictionary

--- functions/cartgetall/main.py
+++ functions/cartgetall/main.py
@@ -21,6 +21,7 @@ def cartgetall(request):
         #conn.commit()
         row = cursor.fetchone()
         while row is not None:
+            results[row[0]] = {}
             results[row[0]]["user_id"] = row[1]
             results[row[0]]["product_id"] = row[2]
             results[row[0]]["amount"] = row[3]
